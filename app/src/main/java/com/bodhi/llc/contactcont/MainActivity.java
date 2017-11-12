02a73553c6dfa52ca4c597e9bb7c1d95391dfa0d
Bodhidipta <bodhidiptabhattacharjee@gmail.com>
2017-11-12 16:13:33 +0530

    Mod

--- app/src/main/java/com/bodhi/llc/contactcont/MainActivity.java
+++ app/src/main/java/com/bodhi/llc/contactcont/MainActivity.java
@@ -77,66 +77,31 @@ public class MainActivity extends AppCompatActivity {
 
 // Defines an array list to contain the ContentValues objects for each row
         ArrayList<ContentValues> contactData = new ArrayList<ContentValues>();
-
-
-/*
- * Defines the raw contact row
- */
-
-// Sets up the row as a ContentValues object
-//        ContentValues rawContactRow = new ContentValues();
-//
-//// Adds the account type and name to the row
-////        rawContactRow.put(ContactsContract.RawContacts.ACCOUNT_TYPE, mSelectedAccount.getType());
-////        rawContactRow.put(ContactsContract.RawContacts.ACCOUNT_NAME, mSelectedAccount.getName());
-//
-//// Adds the row to the array
-//        contactData.add(rawContactRow);
-
-/*
- * Sets up the phone number data row
- */
-
-// Sets up the row as a ContentValues object
         ContentValues phoneRow = new ContentValues();
-
-// Specifies the MIME type for this data row (all data rows must be marked by their type)
         phoneRow.put(
                 ContactsContract.Data.MIMETYPE,
                 ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE
         );
 
-// Adds the phone number and its type to the row
         phoneRow.put(ContactsContract.CommonDataKinds.Phone.NUMBER, phone);
         phoneRow.put(ContactsContract.CommonDataKinds.Phone.TYPE, 1);
-
-// Adds the row to the array
         contactData.add(phoneRow);
-
         ContentValues phoneRow2 = new ContentValues();
-
-// Specifies the MIME type for this data row (all data rows must be marked by their type)
         phoneRow2.put(
                 ContactsContract.Data.MIMETYPE,
                 ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE
         );
-
-// Adds the phone number and its type to the row
         phoneRow2.put(ContactsContract.CommonDataKinds.Phone.NUMBER, "+91 03561 221227");
         phoneRow2.put(ContactsContract.CommonDataKinds.Phone.TYPE,2);
 
-// Adds the row to the array
         contactData.add(phoneRow2);
 
  ContentValues address1 = new ContentValues();
-
-// Specifies the MIME type for this data row (all data rows must be marked by their type)
         address1.put(
                 ContactsContract.Data.MIMETYPE,
                 ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE
         );
 
-// Adds the phone number and its type to the row
         address1.put(ContactsContract.CommonDataKinds.StructuredPostal.STREET, "Adarpara");
         address1.put(ContactsContract.CommonDataKinds.StructuredPostal.CITY,"Jalpaiguri");
         address1.put(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY,"India");
@@ -144,55 +109,30 @@ public class MainActivity extends AppCompatActivity {
         address1.put(ContactsContract.CommonDataKinds.StructuredPostal.REGION,"WestBeng");
         address1.put(ContactsContract.CommonDataKinds.StructuredPostal.TYPE,1);
 
-// Adds the row to the array
         contactData.add(address1);
-
         ContentValues org = new ContentValues();
-
-// Specifies the MIME type for this data row (all data rows must be marked by their type)
         org.put(
                 ContactsContract.Data.MIMETYPE,
                 ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE
         );
-
-// Adds the phone number and its type to the row
-        org.put("Chapter Name", "YPO");
-
-
-// Adds the row to the array
         contactData.add(org);
         ContentValues web = new ContentValues();
-
-// Specifies the MIME type for this data row (all data rows must be marked by their type)
         web.put(
                 ContactsContract.Data.MIMETYPE,
                 ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE
         );
-
-// Adds the phone number and its type to the row
         web.put(ContactsContract.CommonDataKinds.Website.DATA, "www.google.com/bodhidipta");
 
 
-// Adds the row to the array
         contactData.add(web);
-
-/*
- * Sets up the email data row
- */
-
-// Sets up the row as a ContentValues object
         ContentValues emailRow = new ContentValues();
-
-// Specifies the MIME type for this data row (all data rows must be marked by their type)
         emailRow.put(
                 ContactsContract.Data.MIMETYPE,
                 ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE
         );
 
-// Adds the email address and its type to the row
         emailRow.put(ContactsContract.CommonDataKinds.Email.ADDRESS, email);
 
-// Adds the row to the array
         contactData.add(emailRow);
 
 /*
